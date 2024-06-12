0a0caa92659fe64139ebdbe9efc1e32f78320a94
TimSoarer <timomavr@gmail.com>
2024-06-12 16:35:36 +0500

    Добавил функцию телепортации в взаимодействие

--- LysanskyProject/Assets/Scripts/Interactable.cs
+++ LysanskyProject/Assets/Scripts/Interactable.cs
@@ -5,46 +5,102 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+public struct Interaction {
+    public bool questRequired;
+    public ushort questID;
+}
+
 [RequireComponent(typeof(Outline))]
 public class Interactable : MonoBehaviour
 {   
     [Serializable]
     public enum InteractType {None, Teleport, Quest, Shop, Talk};
+    [Header("Basic Properties")]
     public string interactText = "Имя объекта";
     public bool destroyOnInteract = false;
     public InteractType interactType;
+    public float minInteractionRange = 15.0f;
     public GameObject tooltipPrefab;
+    public Vector3 tooltipOffset;
+    [Header("Teleport")]
+    public Vector3 teleportLocation;
+
+    private Camera mainCamera;
+    private GameObject player;
     private GameObject tooltipInstance;
     private Outline outline;
-    public Camera mainCamera;
-    public GameObject canvas;
-    public Vector3 tooltipOffset;
+    private GameObject canvas;
+    
+    bool InRange() {
+        return Vector3.Distance(transform.position, player.transform.position) <= minInteractionRange;
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
         outline = gameObject.GetComponent<Outline>();  
         outline.enabled = false; 
-
+        canvas = GameObject.Find("Canvas");
+        mainCamera = Camera.main;
+        player = mainCamera.gameObject.GetComponent<FollowObject>().characterCapsule.gameObject;
     }
 
     void Update ()
     {
-        if (tooltipInstance != null) {
-            tooltipInstance.transform.position = mainCamera.WorldToScreenPoint(transform.position + tooltipOffset);
+
+    }
+
+    void Interact() {
+        if (interactType == InteractType.Teleport)
+        {
+            player.transform.position = teleportLocation;
         }
     }
 
     void OnMouseEnter()
     {
-        outline.enabled = true; // Enable outline effect
-        tooltipInstance = Instantiate(tooltipPrefab, canvas.transform);
-        tooltipInstance.GetComponent<TextMeshProUGUI>().text = interactText;
+        if (InRange())
+        {
+            outline.enabled = true; // Enable outline effect
+            tooltipInstance = Instantiate(tooltipPrefab, canvas.transform);
+            tooltipInstance.GetComponent<TextMeshProUGUI>().text = interactText;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (InRange())
+        {
+            if (tooltipInstance == null)
+            {
+                outline.enabled = true; // Enable outline effect
+                tooltipInstance = Instantiate(tooltipPrefab, canvas.transform);
+                tooltipInstance.GetComponent<TextMeshProUGUI>().text = interactText;
+            }
+            tooltipInstance.transform.position = mainCamera.WorldToScreenPoint(transform.position + tooltipOffset);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Interact();
+            }
+        }
+        else
+        {
+            if (tooltipInstance != null)
+            {
+                outline.enabled = false; // Disable outline effect
+                Destroy(tooltipInstance);
+                tooltipInstance = null;
+            }
+        }
     }
 
     void OnMouseExit()
     {
-        outline.enabled = false; // Disable outline effect
-        Destroy(tooltipInstance);
-        tooltipInstance = null;
+        if (tooltipInstance != null)
+        {
+            outline.enabled = false; // Disable outline effect
+            Destroy(tooltipInstance);
+            tooltipInstance = null;
+        }
     }
 }
--- LysanskyProject/Assets/Scripts/QuestSystem.cs
+++ LysanskyProject/Assets/Scripts/QuestSystem.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public struct Quest {
+    public string name;
+    public ushort requiredPoints;
+    public bool show;
+}
+
 public class QuestSystem : MonoBehaviour
 {
+    public Quest[] quests;
+    
     // Start is called before the first frame update
     void Start()
     {
